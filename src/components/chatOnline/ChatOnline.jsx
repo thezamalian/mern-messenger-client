8ab0a8d124639222a585f80c578a0d55b0bde5e4
Zamal Hossain <thezamalian@gmail.com>
2022-03-13 12:40:14 +0600

    section for showing online friends is developed

--- src/components/chatOnline/ChatOnline.jsx
+++ src/components/chatOnline/ChatOnline.jsx
@@ -10,6 +10,22 @@ const Chatonline = () => {
                 </div>
                 <span className="chatOnlineName">John Doe</span>
             </div>
+
+            <div className="chatOnlineFriend">
+                <div className="chatOnlineImgContainer">
+                    <img className="chatOnlineImg" src="https://images.unsplash.com/photo-1607990281513-2c110a25bd8c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80" alt="" />
+                    <div className="chatOnlineBadge"></div>
+                </div>
+                <span className="chatOnlineName">John Doe</span>
+            </div>
+            <div className="chatOnlineFriend">
+                <div className="chatOnlineImgContainer">
+                    <img className="chatOnlineImg" src="https://images.unsplash.com/photo-1607990281513-2c110a25bd8c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80" alt="" />
+                    <div className="chatOnlineBadge"></div>
+                </div>
+                <span className="chatOnlineName">John Doe</span>
+            </div>
+
         </div>
     );
 }
